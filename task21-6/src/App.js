163afbb24cf560e96795a5fb181d74b8593c7ad0
roayas <100063786+roayas@users.noreply.github.com>
2022-06-26 23:14:35 +0300

    'task'

--- task21-6/src/App.js
+++ task21-6/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Search from "./Search";
 import Slider from "./Slider";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel';
  function App(){
   return(
--- task21-6/src/Slider.js
+++ task21-6/src/Slider.js
@@ -1,7 +1,11 @@
+import React from 'react';
+
 import Carousel from 'react-bootstrap/Carousel';
 
 function Slider() {
+  
   return (
+    
     <Carousel variant="dark">
       <Carousel.Item>
         <img
